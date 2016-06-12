3744a7895fde6d271359d95c5e988cd64f367434
Gian <Gian@Gi>
2016-06-12 17:16:32 +0800

    Added imports

--- src/java/ServletsUpload/UploadToDatabase.java
+++ src/java/ServletsUpload/UploadToDatabase.java
@@ -7,10 +7,14 @@ package ServletsUpload;
 
 
 
+import Excel.ExcelToHtml;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
  
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -18,6 +22,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
@@ -31,44 +36,52 @@ public class UploadToDatabase extends HttpServlet {
      
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
-        
+        HttpSession session = request.getSession();
         InputStream inputStream = null; // input stream of the upload file
          
-        // obtains the upload file part in this multipart request
-        Part filePart = request.getPart("file");
+            // obtains the upload file part in this multipart request
+            Part filePart = request.getPart("file");
             // obtains input stream of the upload file
             inputStream = filePart.getInputStream();
         
-          POIFSFileSystem fs = new POIFSFileSystem( inputStream );
-          //TODO can be changed into xssf      
-          HSSFWorkbook wb = new HSSFWorkbook(fs);
+            POIFSFileSystem fs = new POIFSFileSystem( inputStream );
+            //TODO can be changed into xssf      
+            HSSFWorkbook wb = new HSSFWorkbook(fs);
+
+            String table = (new ExcelToHtml(wb).getHTML());
+            session.setAttribute("Table", table);
+          
+            ServletContext context= getServletContext();
+            RequestDispatcher rd= context.getRequestDispatcher("/confirmByAgeGroupSex.jsp");
+            rd.forward(request, response);
+
              
-          //@todo change this to user input radio
-          HSSFSheet sheet = wb.getSheetAt(1);
-
-                Iterator rows = sheet.rowIterator();
-                while( rows.hasNext() ) {  
-                    HSSFRow row = (HSSFRow) rows.next();
-                    System.out.println("\n");
-                    Iterator cells = row.cellIterator();
-                    while( cells.hasNext() ) {
-
-                        HSSFCell cell = (HSSFCell) cells.next();
-                        if(HSSFCell.CELL_TYPE_NUMERIC==cell.getCellType())
-                        System.out.print( cell.getNumericCellValue()+"     " );
-                        else
-                        if(HSSFCell.CELL_TYPE_STRING==cell.getCellType())
-                            System.out.print( cell.getStringCellValue()+"     " );
-                        else
-                            if(HSSFCell.CELL_TYPE_BOOLEAN==cell.getCellType())
-                            System.out.print( cell.getBooleanCellValue()+"     " );
-                            else
-                                if(HSSFCell.CELL_TYPE_BLANK==cell.getCellType())
-                                    System.out.print( "BLANK     " );
-                                    else
-                                System.out.print("Unknown cell type");                         
-                    }                        
-                }      
+//          //@todo change this to user input radio
+//          HSSFSheet sheet = wb.getSheetAt(1);
+//
+//                Iterator rows = sheet.rowIterator();
+//                while( rows.hasNext() ) {  
+//                    HSSFRow row = (HSSFRow) rows.next();
+//                    System.out.println("\n");
+//                    Iterator cells = row.cellIterator();
+//                    while( cells.hasNext() ) {
+//
+//                        HSSFCell cell = (HSSFCell) cells.next();
+//                        if(HSSFCell.CELL_TYPE_NUMERIC==cell.getCellType())
+//                        System.out.print( cell.getNumericCellValue()+"     " );
+//                        else
+//                        if(HSSFCell.CELL_TYPE_STRING==cell.getCellType())
+//                            System.out.print( cell.getStringCellValue()+"     " );
+//                        else
+//                            if(HSSFCell.CELL_TYPE_BOOLEAN==cell.getCellType())
+//                            System.out.print( cell.getBooleanCellValue()+"     " );
+//                            else
+//                                if(HSSFCell.CELL_TYPE_BLANK==cell.getCellType())
+//                                    System.out.print( "BLANK     " );
+//                                    else
+//                                System.out.print("Unknown cell type");                         
+//                    }                        
+//                }      
             }
 
 
